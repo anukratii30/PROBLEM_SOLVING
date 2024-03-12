9a69d46bcd6630ee9c96a6a714363612c241c5d7
anura <anura@LAPTOP-U6S7LIMS>
2024-03-12 23:20:11 +0530

    problem solving

--- Problem_Solving/src/Searching2/FindNthrootofM.java
+++ Problem_Solving/src/Searching2/FindNthrootofM.java
@@ -6,5 +6,28 @@ public class FindNthrootofM {
 		// TODO Auto-generated method stub
 
 	}
+	 public static int findPow(int x, int p, int val){
+	        long ans = 1;
+	        for(int i = 0; i<p; i++){
+	            ans *= x;
+	            if(ans > val) return 2;
+	        }
+	        if(ans == val) return 0;
+	        else return 1;
+	    }
+
+	    public int NthRoot(int r, int x){
+	        int low = 1;
+	        int high = x;
+	        while(low <= high){
+	            int mid = (low + high) / 2;
+	            int p = findPow(mid, r, x);
+	            if(p == 0) return mid;
+	            else if(p == 1) low = mid + 1;
+	            else high = mid - 1;
+	        }
+	        return -1;
+	        
+	    }
 
 }
--- Problem_Solving/src/Searching2/Get_Equal_Substrings_Within_Budget.java
+++ Problem_Solving/src/Searching2/Get_Equal_Substrings_Within_Budget.java
@@ -6,5 +6,15 @@ public class Get_Equal_Substrings_Within_Budget {
 		// TODO Auto-generated method stub
 
 	}
+	 public int equalSubstring(String s, String t, int maxCost) {
+		    int j = 0;
+		    for (int i = 0; i < s.length(); ++i) {
+		      maxCost -= Math.abs(s.charAt(i) - t.charAt(i));
+		      if (maxCost < 0)
+		        maxCost += Math.abs(s.charAt(j) - t.charAt(j++));
+		    }
+
+		    return s.length() - j;
+		  }
 
 }
