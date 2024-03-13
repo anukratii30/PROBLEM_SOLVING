035cfd3d7701c7770c12e8921c8f7cadae02640c
anura <anura@LAPTOP-U6S7LIMS>
2024-03-14 00:31:39 +0530

    done

--- Problem_Solving/src/Searching3/Minimum_Limit_of_Balls_in_a_Bag_1760.java
+++ Problem_Solving/src/Searching3/Minimum_Limit_of_Balls_in_a_Bag_1760.java
@@ -1,5 +1,29 @@
 package Searching3;
 
+import java.util.Arrays;
+
 public class Minimum_Limit_of_Balls_in_a_Bag_1760 {
+	 public int minimumSize(int[] nums, int maxOperations) {
+		    int l = 1;
+		    int r = Arrays.stream(nums).max().getAsInt();
+
+		    while (l < r) {
+		      final int m = (l + r) / 2;
+		      if (numOperations(nums, m) <= maxOperations)
+		        r = m;
+		      else
+		        l = m + 1;
+		    }
+
+		    return l;
+		  }
+
+		  // Returns the number of operations required to make m penalty.
+		  private int numOperations(int[] nums, int m) {
+		    int operations = 0;
+		    for (final int num : nums)
+		      operations += (num - 1) / m;
+		    return operations;
+		  }
 
 }
