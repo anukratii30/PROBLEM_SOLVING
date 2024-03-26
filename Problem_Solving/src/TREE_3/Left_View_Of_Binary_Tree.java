8e3dd55242fc673e26c86d501f0e5a53437578c8
anura <anura@LAPTOP-U6S7LIMS>
2024-03-26 17:52:49 +0530

    done

--- Problem_Solving/src/TREE_3/Left_View_Of_Binary_Tree.java
+++ Problem_Solving/src/TREE_3/Left_View_Of_Binary_Tree.java
@@ -6,5 +6,46 @@ public class Left_View_Of_Binary_Tree {
 		// TODO Auto-generated method stub
 
 	}
+	class Node {
+	    int data;
+	    Node left, right;
+	 
+	    public Node(int item)
+	    {
+	        data = item;
+	        left = right = null;
+	    }
+	}
+	 
+	/* Class to print the left view */
+	class BinaryTree {
+	    Node root;
+	    static int max_level = 0;
+	 
+	    // recursive function to print left view
+	    void leftViewUtil(Node node, int level)
+	    {
+	        // Base Case
+	        if (node == null)
+	            return;
+	 
+	        // If this is the first node of its level
+	        if (max_level < level) {
+	            System.out.print(node.data + " ");
+	            max_level = level;
+	        }
+	 
+	        // Recur for left and right subtrees
+	        leftViewUtil(node.left, level + 1);
+	        leftViewUtil(node.right, level + 1);
+	    }
+	
+	    void leftView()
+	    {
+	        max_level = 0;
+	        leftViewUtil(root, 1);
+	    }
+	}
+	 
 
 }
--- Problem_Solving/src/TREE_3/Right_view_of_BT.java
+++ Problem_Solving/src/TREE_3/Right_view_of_BT.java
@@ -1,5 +1,50 @@
 package TREE_3;
 
-public class Right_view_of_BT {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Right_view_of_BT {
+	public static void main(String[] args) {
+
+	}
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+
+		List<Integer> ll = new ArrayList<>();
+		int max = -1;
+
+		public List<Integer> rightSideView(TreeNode root) {
+			dfs(root, 0);
+			return ll;
+		}
+
+		private void dfs(TreeNode root, int lvl) {
+			// TODO Auto-generated method stub
+			if (root == null) {
+				return;
+			}
+			if (lvl > max) {
+				ll.add(root.val);
+				max = lvl;
+			}
+			dfs(root.right, lvl + 1);
+			dfs(root.left, lvl + 1);
+		}
+	}
 }
