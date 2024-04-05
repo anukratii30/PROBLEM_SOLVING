041064958b14ab7dd9e31d411bddb2403b19bbee
anura <anura@192.168.1.13>
2024-04-05 23:57:54 +0530

    done

--- Problem_Solving/src/TREE_4_AND_5/Has_One_child.java
+++ Problem_Solving/src/TREE_4_AND_5/Has_One_child.java
@@ -1,5 +1,17 @@
 package TREE_4_AND_5;
 
 public class Has_One_child {
+	boolean hasOnlyOneChild(int pre[], int size) {
+        int nextDiff, lastDiff;
+ 
+        for (int i = 0; i < size - 1; i++) {
+            nextDiff = pre[i] - pre[i + 1];
+            lastDiff = pre[i] - pre[size - 1];
+            if (nextDiff * lastDiff < 0) {
+                return false;
+            };
+        }
+        return true;
+    }
 
 }
--- Problem_Solving/src/TREE_4_AND_5/converted_sorted_array_to_BST.java
+++ Problem_Solving/src/TREE_4_AND_5/converted_sorted_array_to_BST.java
@@ -1,5 +1,36 @@
 package TREE_4_AND_5;
 
+import Tree_BST.Inserting_into_bst_701.TreeNode;
+
 public class converted_sorted_array_to_BST {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public TreeNode sortedArrayToBST(int[] nums) {
+	    return build(nums, 0, nums.length - 1);
+	  }
+
+	  private TreeNode build(int[] nums, int l, int r) {
+	    if (l > r)
+	      return null;
+	    final int m = (l + r) / 2;
+	    return new TreeNode(nums[m], build(nums, l, m - 1), build(nums, m + 1, r));
+	  }
 
 }
