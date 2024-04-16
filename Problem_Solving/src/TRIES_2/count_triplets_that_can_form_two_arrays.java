8ba14edd6cfea02d0611779f1905403d4d268231
anura <anura@192.168.1.23>
2024-04-16 23:42:19 +0530

    done

--- Problem_Solving/src/TRIES_2/count_triplets_that_can_form_two_arrays.java
+++ Problem_Solving/src/TRIES_2/count_triplets_that_can_form_two_arrays.java
@@ -1,5 +1,16 @@
 package TRIES_2;
 
 public class count_triplets_that_can_form_two_arrays {
+	 public int countTriplets(int[] arr) {
+	        int res = 0;
+	        for(int i=0;i<arr.length-1;i++){
+	            int a = arr[i];
+	            for(int j=i+1;j<arr.length;j++){
+	                a ^= arr[j];
+	                res += a==0 ? j-i:0;
+	            }
+	        }
+	        return res;
+	    }
 
 }
