033e0e47eabd7f2845ba464e6e033f896d46d8ce
anura <anura@192.168.1.20>
2024-05-11 12:17:01 +0530

    done

--- Problem_Solving/src/Graph_6/redundant_connection.java
+++ Problem_Solving/src/Graph_6/redundant_connection.java
@@ -1,5 +1,80 @@
 package Graph_6;
-
+import java.util.Arrays;
 public class redundant_connection {
+	
+
+	class Solution {
+	    int[] parent;
+
+	    public int[] findRedundantDirectedConnection(int[][] edges) {
+	        int n = edges.length;
+	        parent = new int[n + 1];
+	        Arrays.fill(parent, -1);
+
+	        int[] secondEdge = null;
+	        int[] firstEdge = null;
+	        boolean hasTwoParents = false;
+
+	        for (int[] edge : edges) {
+	            int from = edge[0];
+	            int to = edge[1];
+
+	            if (parent[to] == -1) {
+	                parent[to] = from;
+	            } else {
+	                hasTwoParents = true;
+	                secondEdge = edge;
+	                firstEdge = new int[] { parent[to], to };
+	                break;
+	            }
+	        }
+
+	        int[] result = null;
+	        if (!hasTwoParents) {
+	            for (int[] edge : edges) {
+	                int from = edge[0];
+	                int to = edge[1];
+
+	                int leader = find(from);
+	                if (leader == to) {
+	                    result = edge;
+	                    break;
+	                } else {
+	                    parent[to] = leader;
+	                }
+	            }
+	        } else {
+	            boolean isCycle = false;
+	            for (int[] edge : edges) {
+	                if (Arrays.equals(edge, secondEdge))
+	                    continue;
+	                int from = edge[0];
+	                int to = edge[1];
+
+	                int leader = find(from);
+
+	                if (leader == to) {
+	                    isCycle = true;
+	                    break;
+	                } else {
+	                    parent[to] = leader;
+	                }
+	            }
+	            if (isCycle) {
+	                result = firstEdge;
+	            } else {
+	                result = secondEdge;
+	            }
+	        }
+	        return result;
+	    }
+
+	    public int find(int x) {
+	        if (parent[x] == -1)
+	            return x;
+	        return parent[x] = find(parent[x]);
+	    }
+	}
+
 
 }
--- Problem_Solving/src/Graph_6/remove_maximum_number_of_edges.java
+++ Problem_Solving/src/Graph_6/remove_maximum_number_of_edges.java
@@ -1,5 +1,78 @@
 package Graph_6;
 
+import java.util.Arrays;
+
 public class remove_maximum_number_of_edges {
+	class DSU {
+	    int[] p, r;
+
+	    DSU(int n) {
+	        p = new int[n + 1];
+	        r = new int[n + 1];
+	        Arrays.fill(r, 1);
+	        for (int i = 0; i <= n; i++)
+	            p[i] = i;
+	    }
+
+	    int find(int u) {
+	        if (p[u] == u)
+	            return u;
+	        return p[u] = find(p[u]);
+	    }
+
+	    boolean union(int u, int v) {
+	        int pu = find(u);
+	        int pv = find(v);
+	        if (pu == pv)
+	            return false;
+	        if (r[pu] < r[pv]) {
+	            p[pu] = pv;
+	        } else {
+	            p[pv] = pu;
+	            if (r[pu] == r[pv]) {
+	                r[pu]++;
+	            }
+	        }
+	        return true;
+	    }
+	}
+
+	class Solution {
+	    public int maxNumEdgesToRemove(int n, int[][] edges) {
+	        int red = 0, b = 0, a = 0;
+	        DSU ad = new DSU(n);
+	        DSU bd = new DSU(n);
+	        for (int[] e : edges) {
+	            if (e[0] == 3) {
+	                if (ad.union(e[1], e[2])) {
+	                    bd.union(e[1], e[2]);
+	                    b++;
+	                    a++;
+	                } else {
+	                    red++;
+	                }
+	            }
+	        }
+	        for (int[] e : edges) {
+	            if (e[0] == 1) {
+	                if (ad.union(e[1], e[2]))
+	                    a++;
+	                else
+	                    red++;
+	            }
+	            if (e[0] == 2) {
+	                if (bd.union(e[1], e[2]))
+	                    b++;
+	                else
+	                    red++;
+	            }
+	        }
+	        if (b == n - 1 && a == n - 1) {
+	            return red;
+	        }
+	        return -1;
+	    }
+	}
+
 
 }
